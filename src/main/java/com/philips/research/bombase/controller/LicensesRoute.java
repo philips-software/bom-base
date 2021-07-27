93134a47466ee11fa3bcff3b1e272b8eaa479e9d
Timo van de Put <timo.van.de.put@philips.com>
2021-07-27 17:42:36 +0200

    Feat(Backend): Add Rest API to define license curations

---
+++ src/main/java/com/philips/research/bombase/controller/LicensesRoute.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright (c) 2020-2021, Koninklijke Philips N.V., https://www.philips.com
+ * SPDX-License-Identifier: MIT
+ */
+
+package com.philips.research.bombase.controller;
+
+import com.github.packageurl.MalformedPackageURLException;
+import com.github.packageurl.PackageURL;
+import com.philips.research.bombase.core.MetaService;
+import com.philips.research.bombase.core.UnknownPackageException;
+import com.philips.research.bombase.core.license_cleaner.LicenseCleanerService;
+import org.springframework.boot.web.server.WebServerException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import pl.tlinkowski.annotation.basic.NullOr;
+import retrofit2.http.Body;
+
+import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/licenses")
+public class LicensesRoute {
+    private final LicenseCleanerService service;
+
+    LicensesRoute(LicenseCleanerService service) {
+        this.service = service;
+    }
+
+    @PostMapping()
+    void setLicenseCuration(@RequestBody CurationJson body) {
+        if (body.license == null || body.curation==null) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Need license and curation in body");
+        }
+        service.defineCuration(body.license, body.curation);
+    }
+
+    static class CurationJson {
+        @NullOr String license;
+        @NullOr String curation;
+    }
+}
